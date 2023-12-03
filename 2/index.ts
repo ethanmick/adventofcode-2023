e4d4c8547a976b905cd4b00b56a8ac0c399477c2
Ethan Mick <ethan@ethanmick.com>
2023-12-02 19:00:12 -0500

    chore: Code cleanup

--- 2/index.ts
+++ 2/index.ts
@@ -23,12 +23,10 @@ type Game = {
 function parse(str: string): GameInput[] {
   return str.split(/\r?\n/).map((line: string) => {
     const [game, data] = line.trim().split(':')
-    console.log(game)
     const pulls: Pulls = data
       .trim()
       .split('; ')
       .map((pull: string): Pull => {
-        console.log('Split', pull.split(', '))
         return pull.split(', ').map((cubes: string) => {
           const pair = cubes.trim().split(' ')
           return [parseInt(pair[0]), pair[1] as Color]
@@ -43,7 +41,7 @@ function parse(str: string): GameInput[] {
 
 function ofColor(c: Color) {
   return function (pull: Pull): number[] {
-    return pull.filter((p) => p[1] === c).map((p) => p[0]) // .reduce((total, p) => total + p[0], 0)
+    return pull.filter((p) => p[1] === c).map((p) => p[0])
   }
 }
 
@@ -53,39 +51,9 @@ const games: Game[] = parse(input).map((gi) => {
     red: Math.max(...gi.pulls.flatMap(ofColor('red'))),
     green: Math.max(...gi.pulls.flatMap(ofColor('green'))),
     blue: Math.max(...gi.pulls.flatMap(ofColor('blue'))),
-    // red: gi.pulls.reduce(ofColor('red'), 0),
-    // green: gi.pulls.reduce(ofColor('green'), 0),
-    // blue: gi.pulls.reduce(ofColor('blue'), 0),
   }
 })
 
 const powers = games.map((g) => g.blue * g.green * g.red)
 const total = powers.reduce((total, p) => total + p, 0)
 console.log('Total', total)
-
-// const games: Game[] = input.split(/\r?\n/).map((line: string) => {
-//   const [game, data] = line.split(':')
-//   const datums = data
-//     .split(';')
-//     .map((s) => s.split(', ').map((s) => s.split(' ')))
-//   const red = datums.reduce(
-//     (total, pulls) =>
-//       total +
-//       pulls.reduce(
-//         (tot, hand) =>
-//           tot +
-//           hand
-//             .filter((pull) => pull[1] === 'red')
-//             .map((pull) => parseInt(pull[0]))
-//             .reduce((p) => p, 0),
-//         0
-//       ),
-//     0
-//   )
-//   return {
-//     id: parseInt(game.split(/\s/)[1]),
-//     red,
-//   }
-// })
-
-// console.log(games)
