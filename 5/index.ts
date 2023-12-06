a69b1a29237479de08a4d64fa9c424f7d53c9170
Ethan Mick <ethan@ethanmick.com>
2023-12-06 12:20:57 -0500

    Day 5, part 2

--- 5/index.ts
+++ 5/index.ts
@@ -2,43 +2,72 @@ import { readFile } from 'fs/promises'
 
 const read = () => readFile('./input.txt', 'utf8')
 
-type Ranges = {
-  source: number
-  destination: number
-  range: number
+type Range = {
+  min: number
+  max: number
 }
 
-type Mapper = Ranges[]
-type Farm = Mapper[]
+type Mapper = {
+  destination: Range
+  source: Range
+  n: number
+}
 
-const inRange = (n: number, source: number, range: number) =>
-  n >= source && n < source + range
+// Returns the range of r2 that overlaps in r1.
+const overlap = (r1: Range, r2: Range): Range | null => {
+  if (r2.max < r1.min) return null
+  if (r2.min > r1.max) return null
+  return {
+    min: Math.max(r1.min, r2.min),
+    max: Math.min(r1.max, r2.max),
+  }
+}
 
-const getDestination = (n: number, maps: Mapper) => {
-  const m = maps.find((m) => inRange(n, m.source, m.range))
-  return m ? m.destination + (n - m.source) : n
+const mapRange = (m: Mapper, input: Range): Range | null => {
+  const subset = overlap(m.source, input)
+  if (!subset) {
+    return null
+  }
+  const offset = Math.max(m.source.min, subset.min) - m.source.min
+  const length = subset.max - subset.min
+  return {
+    min: m.destination.min + offset,
+    max: m.destination.min + offset + length,
+  }
 }
 
 const parse = async () => {
   const split = (await read()).split('\n\n')
   const [seedsUnparsed, ...unparsedMaps] = split
+  // Seed Generators
   const seeds = seedsUnparsed.split(':')[1].trim().split(/\s+/).map(Number)
-  const maps = unparsedMaps.map((raw) => {
+  const generators: Range[] = []
+  while (seeds.length) {
+    const s = seeds.shift() as number
+    const r = seeds.shift() as number
+    generators.push({ min: s, max: s + r })
+  }
+  // Mappers
+  const maps: Mapper[][] = unparsedMaps.map((raw) => {
     const [_, ...inputs] = raw.split('\n')
-    return inputs
-      .flatMap((line) => {
-        const [destination, source, range] = line.split(/\s+/).map(Number)
-        return {
-          source: Number(source),
-          destination: Number(destination),
-          range: Number(range),
-        }
-      })
-      .sort((a, b) => a.source - b.source)
+    return inputs.flatMap((line) => {
+      const [destination, source, n] = line.split(/\s+/).map(Number)
+      return {
+        source: {
+          min: Number(source),
+          max: Number(source) + Number(n),
+        },
+        destination: {
+          min: Number(destination),
+          max: Number(destination) + Number(n),
+        },
+        n: Number(n),
+      } as Mapper
+    })
   })
 
   return {
-    seeds,
+    seeds: generators,
     maps,
   }
 }
@@ -46,15 +75,18 @@ const parse = async () => {
 const main = async () => {
   const { seeds, maps } = await parse()
 
-  const locations = seeds.map((s) => {
-    let l = s
-    for (let m of maps) {
-      l = getDestination(l, m)
+  const all: Range[] = []
+  for (const seed of seeds) {
+    let ranges: Range[] = [seed]
+    for (const m of maps) {
+      ranges = ranges.flatMap(
+        (r) => m.map((m) => mapRange(m, r)).filter((n) => !!n) as Range[]
+      )
     }
-    return l
-  })
+    all.push(...ranges)
+  }
 
-  console.log(Math.min(...locations))
+  console.log('Mininum Location', Math.min(...all.map((r) => r.min)))
 }
 
 main()
