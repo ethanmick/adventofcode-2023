e541456c3ee5bb6039f2017cfe4a97ea40b179c6
Ethan Mick <ethan@ethanmick.com>
2023-12-11 22:17:29 -0500

    Part 2

--- 9/index.ts
+++ 9/index.ts
@@ -15,16 +15,14 @@ const next = (arr: number[]): number[] => {
 
   const [_, ...calc] = arr
   const diffs = calc.map((n, i) => n - arr[i])
-  const last = next(diffs).pop() as number
-  return [...arr, arr[arr.length - 1] + last]
+  const first = next(diffs)[0] as number
+  return [arr[0] - first, ...arr]
 }
 
 const main = async () => {
   const inputs = await parse()
   const updated = inputs.map(next)
-  const total = updated
-    .map((u) => u[u.length - 1])
-    .reduce((total, n) => total + n, 0)
+  const total = updated.map((u) => u[0]).reduce((total, n) => total + n, 0)
   console.log('Total', total)
 }
 
